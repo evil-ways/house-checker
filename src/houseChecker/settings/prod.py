9f1068b152efa6cee2c486d7d18d930f140f0623
goncalo <goncalo@seedstars.com>
2019-11-03 12:08:39 +0000

    add allowed hosts

--- src/houseChecker/settings/prod.py
+++ src/houseChecker/settings/prod.py
@@ -4,6 +4,9 @@ from decouple import config
 from houseChecker.settings.base import *
 
 SECRET_KEY = config('SECRET_KEY')
+
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
