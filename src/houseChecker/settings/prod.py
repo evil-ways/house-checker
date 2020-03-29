24aa39244d0a539ff9f3c771844699d938428e09
goncalo <goncalo@seedstars.com>
2020-03-29 16:43:23 +0100

    Fix celery #2

--- src/houseChecker/settings/prod.py
+++ src/houseChecker/settings/prod.py
@@ -14,6 +14,6 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
 
 REDIS_URL = config('REDIS_URL')
-CELERY_BROKER_URL = REDIS_URL
+CELERY_BROKER_URL = CELERY_RESULT_BACKEND = REDIS_URL
 
 sentry_sdk.init(dsn=config('SENTRY_URL', cast=str), integrations=[DjangoIntegration()])
