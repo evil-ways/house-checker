ef2dfaeb42ed63ac46523306fe8821daf5536401
goncalo <goncalo@seedstars.com>
2020-10-05 23:44:25 +0100

    Add log test

--- src/website/views.py
+++ src/website/views.py
@@ -2,10 +2,14 @@ from django.views.generic import TemplateView
 
 from website.tasks import test_task
 
+import logging
+
+LOGGER = logging.getLogger()
 
 class IndexView(TemplateView):
     template_name = 'website/pages/index.html'
 
     def get(self, request, *args, **kwargs):
         test_task.delay()
+        LOGGER.debug('batataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas')
         return super(IndexView, self).get(request, args, kwargs)
